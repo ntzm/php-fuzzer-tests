0e39a574e475d8550003882f9e0730b904133b68
Nat Zimmermann <nat@ntzm.io>
2020-01-14 19:49:31 +0000

    doctrine dql

---
+++ doctrine-dql/target.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \PhpFuzzer\Fuzzer $fuzzer */
+
+require __DIR__ . '/vendor/autoload.php';
+
+$config = \Doctrine\ORM\Tools\Setup::createAnnotationMetadataConfiguration(['/empty'], true);
+$em = \Doctrine\ORM\EntityManager::create([
+    'driver' => 'pdo_sqlite',
+], $config);
+
+$fuzzer->setTarget(function (string $input) use ($em) {
+    $em->createQuery($input)->execute();
+});
